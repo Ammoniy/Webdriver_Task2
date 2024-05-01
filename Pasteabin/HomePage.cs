fbdee68acde388dd6fd5c1682dd174ab9dfe82f1
Oleksandr Yankovskiy <oankovskij13@gmail.com>
2024-05-01 21:35:19 +0300

    Add project files.

---
+++ Pasteabin/HomePage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Pasteabin
+{
+    public class HomePage
+    {
+        private readonly IWebDriver driver;
+
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public void Paste(string paste_text, string format_text, string exp_text, string title_text)
+        {
+            // click on paste button
+            driver.FindElement(By.XPath("//A[@class='header__btn']")).Click();
+            // fill paste field
+            driver.FindElement(By.XPath("//TEXTAREA[@id='postform-text']")).SendKeys(paste_text);
+            // scroll
+            IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            js.ExecuteScript("window.scrollBy(0,700)");
+            
+            // chose formating
+            var format_dropdown = driver.FindElement(By.XPath("//*[contains(@class, 'field-postform-format')]//span"));
+            format_dropdown.Click();
+            var format_dropdownOption = driver.FindElements(By.XPath("//li[@class='select2-results__option']"));
+            format_dropdownOption.FirstOrDefault(x => x.Text.Equals(format_text)).Click();
+
+
+            // chose expiration from dropdown
+            var exp_dropdown = driver.FindElement(By.XPath("//*[contains(@class, 'field-postform-expiration')]//span"));
+            exp_dropdown.Click();
+            var exp_dropdownOption = driver.FindElements(By.XPath("//li[@class='select2-results__option']"));
+            exp_dropdownOption.FirstOrDefault(x => x.Text.Equals(exp_text)).Click();
+            
+            // fill Paste Name / Title
+            driver.FindElement(By.XPath("//INPUT[@id='postform-name']")).SendKeys(title_text);
+            
+            // create new paste
+            driver.FindElement(By.XPath("//BUTTON[@type='submit'][text()='Create New Paste']")).Click();
+
+        }
+    }
+}
---
+++ Webdriver_Task2/PasteDataValidation.cs
@@ -0,0 +1,75 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Pasteabin;
+
+namespace Webdriver_Task2
+{
+    public class Tests
+    {
+
+        #region data
+
+        IWebDriver driver;
+        readonly string paste = @"git config --global user.name  ""New Sheriff in Town""
+git reset $(git commit-tree HEAD^{tree} -m ""Legacy code"")
+git push origin master --force";
+        readonly string title = "how to gain dominance among developers";
+        readonly string format = "Bash";
+        readonly string expiration = "10 Minutes";
+
+        #endregion
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+
+        }
+
+        [Test]
+        public void Paste_Creation_Validate_Title_Test()
+        {
+            HomePage hp = new HomePage(driver);
+            driver.Url = "https://pastebin.com";
+            hp.Paste(paste_text: paste,
+            format_text: format,
+            exp_text: expiration,
+            title_text: title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            string actualPaste = driver.FindElement(By.ClassName("info-top")).Text;
+            Assert.AreEqual(title, actualPaste);
+        }
+
+        [Test]
+        public void Paste_Creation_Validate_Paste_Test()
+        {
+            HomePage hp = new HomePage(driver);
+            driver.Url = "https://pastebin.com";
+            hp.Paste(paste_text: paste,
+            format_text: format,
+            exp_text: expiration,
+            title_text: title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            string actualPaste = driver.FindElement(By.ClassName("bash")).Text;
+            Assert.AreEqual(paste, actualPaste);
+        }
+
+        [Test]
+        public void Paste_Creation_Validate_Format_Test()
+        {
+            HomePage hp = new HomePage(driver);
+            driver.Url = "https://pastebin.com";
+            hp.Paste(paste_text: paste,
+            format_text: format,
+            exp_text: expiration,
+            title_text: title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            string actualPaste = driver.FindElement(By.ClassName("left")).Text;
+            Assert.IsTrue(actualPaste.Contains(format));
+       }
+    }
+}
